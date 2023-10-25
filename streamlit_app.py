45918b47dc4a9c40fc3e13dc8ead90151fe1f036
Aarushi Kumar <71340243+aarux-11@users.noreply.github.com>
2023-10-26 01:10:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 # SETTING PAGE CONFIGURATIONS
 st.set_page_config(page_title= "Youtube Data Project - Aarushi",
+                   page_icon = "chart_with_upwards_trend",
                    layout= "wide",
                    initial_sidebar_state= "expanded",
                    menu_items={'About': """This project is for submission purposes to GUVI only"""})
