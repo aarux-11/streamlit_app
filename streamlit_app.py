fad8c626a2e8aa433feb96f565751dc14cabef6a
Aarushi Kumar <71340243+aarux-11@users.noreply.github.com>
2023-10-26 01:15:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 
 # SETTING PAGE CONFIGURATIONS
 st.set_page_config(page_title= "Youtube Data Project - Aarushi",
@@ -8,15 +8,15 @@ st.set_page_config(page_title= "Youtube Data Project - Aarushi",
                    initial_sidebar_state= "expanded",
                    menu_items={'About': """This project is for submission purposes to GUVI only"""})
 
-# CREATING OPTION MENU
-with st.sidebar:
-    selected = option_menu(None, ["Home","Extract & Transform","View"], 
-                           icons=["house-door-fill","tools","card-text"],
-                           default_index=0,
-                           orientation="vertical",
-                           styles={"nav-link": {"font-size": "20px", "text-align": "centre", "margin": "0px", 
-                                                "--hover-color": "#A72283"},
-                                   "icon": {"font-size": "20px"},
-                                   "container" : {"max-width": "5000px"},
-                                   "nav-link-selected": {"background-color": "#A72283"}})
+# # CREATING OPTION MENU
+# with st.sidebar:
+#     selected = option_menu(None, ["Home","Extract & Transform","View"], 
+#                            icons=["house-door-fill","tools","card-text"],
+#                            default_index=0,
+#                            orientation="vertical",
+#                            styles={"nav-link": {"font-size": "20px", "text-align": "centre", "margin": "0px", 
+#                                                 "--hover-color": "#A72283"},
+#                                    "icon": {"font-size": "20px"},
+#                                    "container" : {"max-width": "5000px"},
+#                                    "nav-link-selected": {"background-color": "#A72283"}})
 
