cb7ef0cde152e73ca326a6a256eb43181cf2811f
Aarushi Kumar <71340243+aarux-11@users.noreply.github.com>
2023-10-28 16:19:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ ca = certifi.where()
 
 def get_database():
     CONNECTION_STRING = "mongodb+srv://Aarushi:'%40%40rushi1108'@youtubeproject.nrit3zy.mongodb.net/?retryWrites=true&w=majority"
-    client = MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where(), tlsAllowInvalidCertificates=True,ssl_cert_reqs=ssl.CERT_NONE, server_api=ServerApi('1'))
+    client = MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where(), tlsAllowInvalidCertificates=True, server_api=ServerApi('1'))
     
     return client['YT_DataFetch']
 
